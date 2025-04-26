aa6a14cbca0f76a1cf3e261fef396e3ba0740589
Younes <youneshamishebahar5@gmail.com>
2025-04-26 15:29:23 +1000

    inits are added one test run performed and it worked

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from .datasets import *
+from .evaluation import *
+from .models import *
+from .training import *
+from .utils import *
---
+++ src/models/__init__.py
@@ -0,0 +1,2 @@
+from .model import *
+from .classification_head import *
