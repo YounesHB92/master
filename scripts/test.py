96f6000e4df054c2dbf05ebaf9b2bd12c698e88e
Younes <youneshamishebahar5@gmail.com>
2025-05-16 00:21:19 +1000

    feature cleaning done, shape classififer model started

--- scripts/test.py
+++ scripts/test.py
@@ -1,8 +1,4 @@
-from src.features import FeatureExtractor
-from pprint import pprint
+from src.models import ShapeClassifier
 
-feature_ex_ = FeatureExtractor(print_=False, save=True, saving_name="test")
-feature_ex_.run_all(stopper=10)
 
-print(feature_ex_.features)
-
+shape_cl_ = ShapeClassifier(features_file="features_clean.csv")
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,3 @@
 from .load_model import *
 from .classification_head import *
+from .shape_classifier import *
